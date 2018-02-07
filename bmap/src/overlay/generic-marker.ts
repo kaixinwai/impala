c062beffb0ae705329df3793a9c27d1a949c2099
shenfw1987 <shenfw1987@gmail.com>
2018-02-07 14:55:42 +0800

    clean up

--- bmap/src/overlay/generic-marker.ts
+++
@@ -1,7 +0,0 @@
-export class GenericMarker<T> extends BMap.Marker {
-
-    constructor(point: BMap.Point, opts: BMap.MarkerOptions, public data: T) {
-        super(point, opts);
-    }
-
-}
--- bmap/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="./bmap-sdk.d.ts" />
-
-export * from './bmap.directive';
-export * from './bmap.module';
-export * from './bmap-config';
-export * from './overlay/generic-marker';
-export * from './overlay/portal-host-overlay';
-export * from './util/wgs84tobd09';
